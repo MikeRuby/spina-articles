13da54cc01bd3dfcfe809f60ad12745b9f0d70c7
ezbreaks <ezbreaks@users.noreply.github.com>
2016-07-29 17:51:18 +0300

    Category Id association

--- lib/generators/spina_articles/templates/add_spina_category_id_to_spina_articles.rb
+++ lib/generators/spina_articles/templates/add_spina_category_id_to_spina_articles.rb
@@ -1,5 +1,5 @@
 class AddSpinaCategoryIdToSpinaArticles < ActiveRecord::Migration[5.0]
   def change
-    add_column :spina_articles, :spina_category_id, :integer
+    add_column :spina_articles, :category_id, :integer
   end
 end
